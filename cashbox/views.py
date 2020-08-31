7fcfaf1061afa8891fb2256a055a9475152c28fa
Cedrick CF <ce.candia@icloud.com>
2020-08-31 14:03:04 +0200

    init

--- cashbox/views.py
+++ cashbox/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import render
 
+
 # -------------------------------------------------------------------------
 # cashbox
 
 # who can access:
-    # --> cashbox only
+# --> cashbox only
 # what i need:
-    # a list of all the products + services
-    # a list of all categories
-    # a list of all the payment-options
+# a list of all the products + services
+# a list of all categories
+# a list of all the payment-options
 
 # -------------------------------------------------------------------------
 # views:
@@ -16,8 +17,10 @@ from django.shortcuts import render
 def cashbox_dashboard_view(request, *args, **kwargs):
     return render(request, "cashbox_dashboard.html", {})
 
+
 def cashbox_pay_view(request, *args, **kwargs):
     return render(request, "cashbox_pay.html", {})
+
 
 def cashbox_more_view(request, *args, **kwargs):
     return render(request, "cashbox_more.html", {})
