1d6929f7d6715e22f08f26ef1b7145f3507a90d8
Abentrix <59092235+Abentrix@users.noreply.github.com>
2020-09-05 16:37:22 +0200

    New Forms

--- cashbox/forms.py
+++ cashbox/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from .models import *
-
+styling = ""
 
 class FormCreateReversalBill(forms.Form):
     amount = forms.IntegerField(widget=forms.TextInput(attrs={"class": "form-row"}))
