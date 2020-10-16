d95fbd298ee9c88a145e68af357cf4a04155629b
ludwigbe <ludwig.bergemann3004@gmail.com>
2020-10-16 14:22:31 +0200

    error solving
    diese pycache dateien kannman denke ich ignorieren

--- customer/forms.py
+++ customer/forms.py
@@ -1,29 +1,36 @@
 from django import forms
-from .models import *
+from .models import Customer    
 styling = ""
 
 class FormCreateEditCustomer(forms.ModelForm):
-        firstname = forms.CharField(required=True,
-                             label='',
-                             widget=forms.TextInput(
-                                 attrs={"placeholder": "",
-                                        "class": ""
-                                        }))
-        lastname = forms.CharField(required=True,
-                             label='',
-                             widget=forms.TextInput(
-                                 attrs={"placeholder": "",
-                                        "class": ""
-                                        }))
-        phone_number = forms.CharField(required=True,
-                             label='',
-                             widget=forms.NumberInput(
-                                 attrs={"placeholder": "",
-                                        "class": ""
-                                        }))
-        notes = forms.CharField(required=True,
-                             label='',
-                             widget=forms.TextInput(
-                                 attrs={"placeholder": "",
-                                        "class": ""
-                                        }))
+
+    class Meta:
+        model = Customer
+        fields = ['firstname', 'lastname', 'phonenumber', 'notes']
+
+    firstname = forms.CharField(required=True,
+                            label='',
+                            widget=forms.TextInput(
+                                attrs={"placeholder": "",
+                                    "class": ""
+                                    }))
+    lastname = forms.CharField(required=True,
+                            label='',
+                            widget=forms.TextInput(
+                                attrs={"placeholder": "",
+                                    "class": ""
+                                    }))
+    phone_number = forms.CharField(required=True,
+                            label='',
+                            widget=forms.NumberInput(
+                                attrs={"placeholder": "",
+                                    "class": ""
+                                    }))
+    notes = forms.CharField(required=True,
+                            label='',
+                            widget=forms.TextInput(
+                                attrs={"placeholder": "",
+                                    "class": ""
+                                    }))
+
+
