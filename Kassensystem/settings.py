8b40b78c7dcadef619d3c5839c7ccd1d0504cea0
Maximilian Huber <52167836+maxivhuber@users.noreply.github.com>
2020-10-30 18:06:01 +0100

    rr

--- Kassensystem/settings.py
+++ Kassensystem/settings.py
@@ -94,23 +94,17 @@ WSGI_APPLICATION = 'Kassensystem.wsgi.application'
 #Database
 #https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': '07yp3juew2',
-#         'USER': 'd8qd3cnm31',
-#         'PASSWORD': 'g6ipo5g3yt3chgctcnloy9762',
-#         'HOST': 'cloudrooter.ddns.net',
-#         'PORT': '3306',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': str(os.path.join(BASE_DIR, "db.sqlite3")),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': '07yp3juew2',
+        'USER': 'd8qd3cnm31',
+        'PASSWORD': 'g6ipo5g3yt3chgctcnloy9762',
+        'HOST': 'cloudrooter.ddns.net',
+        'PORT': '3306',
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
